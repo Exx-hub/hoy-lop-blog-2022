f725b564957208e792832e419580c747b679ccdd
Alvin Acosta <alvin.acosta@movon.com.ph>
2023-01-11 14:48:17 +0800

    update footer year to 2023

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/Footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <h6>© 2022 Pauline Ponciano </h6>
+      <h6>© 2023 Pauline Ponciano </h6>
     </footer>
   );
 }
